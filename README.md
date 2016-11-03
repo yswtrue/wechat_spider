e2a3365abade0e371515f22c6862e467cae88eaa
sundyli <543950155@qq.com>
2016-11-03 10:45:46 +0800

    add win client script

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # wechat_spider
 微信公众号爬虫  (只需设置代理, 一键可以爬取指定公众号的所有历史文章)
 
-- 一个简单的Demo  [simple_server.go][1]
+代理服务端: 通过Man-In-Middle 代理方式获取微信服务端返回, 自动模拟请求自动分页,抓取对应点击的所有历史文章
 
+客户端:   暂时支持 win,macos,android三大平台,  iphone由于https证书问题后续再支持
+
+#### 代理服务端
+- 一个简单的Demo  [simple_server.go][1]
 
 ```
 package main
@@ -30,16 +34,23 @@ func main() {
 }
 ```
 
-
-
-- 使用方法:
-运行后, 设置手机的代理为 本机ip 8899端口,  打开微信客户端, 点击任一公众号查看历史文章按钮, 即可爬取该公众号的所有历史文章(已经支持自动翻页爬取)
-
-
 - 自定义输出源,实现Processor接口的Output方法即可, [custom_output_server.go][2]
 
 
-  [1]: https://github.com/sundy-li/wechat_spider/blob/master/examples/simple_server.go
-  [2]: https://github.com/sundy-li/wechat_spider/blob/master/examples/custom_output_server.go
+[1]: https://github.com/sundy-li/wechat_spider/blob/master/examples/simple_server.go
+[2]: https://github.com/sundy-li/wechat_spider/blob/master/examples/custom_output_server.go
 
 - 微信会屏蔽频繁的请求,所以历史文章的翻页请求调用了Sleep()方法, 默认每个请求休眠50ms,可以根据实际情况自定义Processor覆盖此方法
+
+
+#### 客户端使用:    
+  (确保客户端 能正常访问 代理服务端的服务) 
+
+- Android客户端使用方法:
+  运行后, 设置手机的代理为 本机ip 8899端口,  打开微信客户端, 点击任一公众号查看历史文章按钮, 即可爬取该公众号的所有历史文章(已经支持自动翻页爬取)
+- win/mac客户端,设置下全局代理对应 代理服务端的服务和端口,同理点击任一公众号查看历史文章按钮
+- 自动化批量爬取所有公众号:  Windows客户端获取批量公众号所有历史文章方法
+  1. 要求安装windows +  微信pc版本 + ActivePython3 + autogui, 设置windows下全局代理对应 代理服务端的服务和端口
+  2. 修改 win_client.py 中的 bizs参数, 通过pyautogui.position() 瞄点设置 first_ret, rel_link 坐标
+  3. 执行 python win_client.py 将自动生成链接,模拟点击
+
---
+++ examples/win_client.py
@@ -0,0 +1,62 @@
+#-*- coding: utf-8 -*-   
+
+# Thanks to @superalsrk 
+
+# The encode may be gb18030 to varing operation system
+# Download ActivePython-3.4.3.2-win64-x64.msi and install
+# Use pypm to install pyautogui and requests
+# Run :  python pc_client.py
+
+import win32gui
+import win32api
+import win32con
+import time
+import ctypes
+import requests
+import pyautogui
+import datetime
+
+print(u'Start..')
+counter = 1 
+
+## fetch the mouse position by  pyautogui.position()  
+first_ret = (666, 666)
+rel_link = (888, 464)
+
+
+## 通过查看历史文章的页面链接获取到对应的bizid
+bizs = ["MzA5NzEyMTQ2NQ==", "MzIzMDE0MDQyNQ==", "MjM5MDEyMDk4Mw=="]
+
+def gen():
+   global counter
+   return bizs[counter % len(bizs)]
+
+def process():
+   global counter
+   print('Requests Number:  %d  Biz (%s)\n----------------------------------------------------' % (counter,  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+   
+   print(pyautogui.size())
+   print(pyautogui.position())
+   
+   ## 获取句柄
+   hwnd = win32gui.FindWindow(None, u"微信")
+   win32gui.SetForegroundWindow(hwnd)
+   win32gui.SetForegroundWindow(hwnd)
+   
+   pyautogui.moveTo(x=first_ret[0], y=first_ret[1], duration=1)
+   pyautogui.click(x=first_ret[0], y = first_ret[1])
+
+   biz = gen()
+
+   pyautogui.typewrite(" http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=%s==#wechat_webview_type=1&wechat_redirect" % biz, interval=0)
+
+   pyautogui.keyDown('enter')
+   pyautogui.moveTo(rel_link[0], rel_link[1], duration=1)
+   pyautogui.click(x=rel_link[0], y=rel_link[1])
+   counter = counter + 1
+   
+   
+if __name__ == '__main__':
+   while True:
+      process()
+      time.sleep(3)
